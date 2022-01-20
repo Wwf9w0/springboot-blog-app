df204259482f6b3530aeb31af530dd814f657dde
wwf9w0 <aliemrekaradag1@gmail.com>
2022-01-20 19:16:33 +0300

    added rendertohtml

---
+++ src/main/java/com/blog/app/support/FlexMarkdownService.java
@@ -0,0 +1,30 @@
+package com.blog.app.support;
+
+import com.vladsch.flexmark.ext.autolink.AutolinkExtension;
+import com.vladsch.flexmark.ext.gfm.strikethrough.StrikethroughExtension;
+import com.vladsch.flexmark.ext.tables.TablesExtension;
+import com.vladsch.flexmark.html.HtmlRenderer;
+import com.vladsch.flexmark.parser.Parser;
+import com.vladsch.flexmark.util.ast.Node;
+import com.vladsch.flexmark.util.data.MutableDataSet;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+
+@Service("flexmark")
+@Slf4j
+public class FlexMarkdownService {
+    public String renderToHtml(String content){
+        MutableDataSet options = new MutableDataSet();
+        options.set(Parser.EXTENSIONS,
+                Arrays.asList(TablesExtension.create(),
+                        AutolinkExtension.create(),
+                        StrikethroughExtension.create()));
+
+        Parser parser = Parser.builder(options).build();
+        HtmlRenderer renderer = HtmlRenderer.builder(options).build();
+        Node document = parser.parse(content);
+        return renderer.render(document);
+    }
+}
