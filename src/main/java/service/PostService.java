7b9b80517a797cda317ed08190538ac4713cda87
wwf9w0 <aliemrekaradag1@gmail.com>
2022-01-22 20:41:24 +0300

    added get tag names

--- src/main/java/service/PostService.java
+++ src/main/java/service/PostService.java
@@ -138,6 +138,15 @@ public class PostService {
         return tags;
     }
 
+    public String getTagNames(Set<Tag> tags){
+        if (!Objects.nonNull(tags) || tags.isEmpty()){
+            return "";
+        }
+        StringBuilder names = new StringBuilder();
+        tags.forEach(tag -> names.append(tag.getName()).append(","));
+        names.deleteCharAt(names.length() - 1);
 
+        return names.toString();
+    }
 
 }
