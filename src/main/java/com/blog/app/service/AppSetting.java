bdf8d0c962cb812b85ffc528b491b49063c380c4
wwf9w0 <aliemrekaradag1@gmail.com>
2022-01-30 16:06:19 +0300

    added appsettings get set methods

--- src/main/java/com/blog/app/service/AppSetting.java
+++ src/main/java/com/blog/app/service/AppSetting.java
@@ -8,9 +8,40 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Slf4j
 public class AppSetting {
+
+    private final SettingsService settingService;
+
     public static final String SITE_NAME = "site_name";
     public static final String SITE_SLOGAN = "site_slogan";
     public static final String PAGE_SIZE = "page_size";
+    private String siteName = "SpringBlog";
+    private String siteSlogan = "An interesting place to discover";
+    private Integer pageSize = 5;
 
+    public String getSiteName() {
+        return (String) settingService.get(SITE_NAME, siteName);
+    }
 
+    public void setSiteName(String siteName) {
+        this.siteName = siteName;
+        settingService.put(SITE_NAME, siteName);
+    }
+
+    public Integer getPageSize() {
+        return (Integer) settingService.get(PAGE_SIZE, pageSize);
+    }
+
+    public void setPageSize(Integer pageSize) {
+        this.pageSize = pageSize;
+        settingService.put(PAGE_SIZE, pageSize);
+    }
+
+    public String getSiteSlogan() {
+        return (String) settingService.get(SITE_SLOGAN, siteSlogan);
+    }
+
+    public void setSiteSlogan(String siteSlogan) {
+        this.siteSlogan = siteSlogan;
+        settingService.put(SITE_SLOGAN, siteSlogan);
+    }
 }
