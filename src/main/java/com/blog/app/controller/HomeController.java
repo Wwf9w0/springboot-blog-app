949295e7468d7b89d79c73ea2c47ed192b489ffc
wwf9w0 <aliemrekaradag1@gmail.com>
2022-02-05 13:58:08 +0300

    created homecontroller and getPosts method

---
+++ src/main/java/com/blog/app/controller/HomeController.java
@@ -0,0 +1,30 @@
+package com.blog.app.controller;
+
+import com.blog.app.model.Post;
+import com.blog.app.service.AppSetting;
+import com.blog.app.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.xml.ws.Response;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class HomeController {
+    private final PostService postService;
+    private final AppSetting appSetting;
+
+    @GetMapping
+    public ResponseEntity<Page<Post>> getPosts(@PathVariable int page){
+        page = page < 1  ? 0 : page -1 ;
+        Page<Post> posts = postService.getAllPublishedPostsByPage(page, appSetting.getPageSize());
+        return ResponseEntity.ok(posts);
+    }
+
+}
