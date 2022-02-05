021d36adf8b5363555ef2ff7a80b4e2c2fa97b8d
wwf9w0 <aliemrekaradag1@gmail.com>
2022-02-05 17:06:42 +0300

    refactor

--- src/main/java/com/blog/app/controller/UserController.java
+++ src/main/java/com/blog/app/controller/UserController.java
@@ -1,11 +1,9 @@
 package com.blog.app.controller;
 
-import com.blog.app.model.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.xml.ws.Response;
 import java.security.Principal;
 
 @RestController
