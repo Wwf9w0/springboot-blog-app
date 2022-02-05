d1b6330f97a134ad241849714f18f80bbb3b348e
wwf9w0 <aliemrekaradag1@gmail.com>
2022-02-05 13:58:24 +0300

    refactor

--- src/main/java/com/blog/app/controller/HomeController.java
+++ src/main/java/com/blog/app/controller/HomeController.java
@@ -8,11 +8,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.xml.ws.Response;
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
