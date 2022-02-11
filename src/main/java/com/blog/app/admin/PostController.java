edb0416bba0e8df1a34ef675d72068df50018358
wwf9w0 <aliemrekaradag1@gmail.com>
2022-02-12 00:11:18 +0300

    refactor

--- src/main/java/com/blog/app/admin/PostController.java
+++ src/main/java/com/blog/app/admin/PostController.java
@@ -1,16 +1,12 @@
 package com.blog.app.admin;
 
-import com.blog.app.model.Post;
-import com.blog.app.model.forms.PostForm;
 import com.blog.app.repository.PostRepository;
 import com.blog.app.service.PostService;
 import com.blog.app.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.xml.ws.Response;
 
 @RestController
 @RequiredArgsConstructor
